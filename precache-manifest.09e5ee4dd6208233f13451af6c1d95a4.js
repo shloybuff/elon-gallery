fde78164ab46be2e278146d8a060b4a3e21f0dbd
chloy <buffshloy55@gmail.com>
2020-06-30 09:12:24 +0200

    Updates

---
+++ precache-manifest.09e5ee4dd6208233f13451af6c1d95a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5993ac2812372d7d5fcccbcfbca7c211",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac7d49c13e5ec2dff489",
+    "url": "/static/css/main.5eaef3dc.chunk.css"
+  },
+  {
+    "revision": "311e3b96446a66bdd460",
+    "url": "/static/js/2.4e7edd87.chunk.js"
+  },
+  {
+    "revision": "9730e669d0d0294186bddd85a3fbb5e4",
+    "url": "/static/js/2.4e7edd87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac7d49c13e5ec2dff489",
+    "url": "/static/js/main.e9f50cee.chunk.js"
+  },
+  {
+    "revision": "8b32aed7646c49e15168",
+    "url": "/static/js/runtime-main.12d61eb2.js"
+  }
+]);
